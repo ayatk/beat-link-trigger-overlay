ddecac28b5d2eea15113be3530c98ef8e58d1592
ayatk <git@ayatk.com>
2021-09-08 13:33:43 +0900

    アニメーションするようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Track from './components/Track'
 import { defaultApi } from './fetch'
+import { useState } from 'react'
 import { useQuery } from 'react-query'
 
 const App = (): JSX.Element => {
@@ -7,16 +8,22 @@ const App = (): JSX.Element => {
     return await defaultApi.paramsJsonGet()
   })
 
+  const [playing, setPlaying] = useState(false)
+
   if (isLoading) {
     return <div>Loading...</div>
   }
 
   return (
-    <Track
-      title={data.master.track.title}
-      artist={data.master.track.artist}
-      imageUrl={`/artwork/${data.master.number}`}
-    />
+    <>
+      <Track
+        playing={playing}
+        title={data.players['1']?.track.title}
+        artist={data.players['1']?.track.artist}
+        imageUrl={`/artwork/${data.players['1']?.number}`}
+      />
+      <button onClick={() => setPlaying(!playing)}>Play: {playing.toString()}</button>
+    </>
   )
 }
 
--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -1,25 +1,111 @@
-import styled from 'styled-components'
+import { SlideLeft } from '../../keyframe'
+import { useMachine } from '@xstate/react'
+import { useEffect } from 'react'
+import styled, { css } from 'styled-components'
+import { createMachine, State } from 'xstate'
 
 export interface Props {
+  playing: boolean
   title: string
   artist: string
   imageUrl: string
 }
 
-const Track = ({ title, artist, imageUrl }: Props): JSX.Element => {
+type TransitionContext = unknown
+
+type TransitionState = State<TransitionContext, TransitionEvent, TransitionTypestate>
+
+type TransitionEvent = { type: 'NEXT' } | { type: 'END' } | { type: 'PLAY' } | { type: 'STOP' }
+
+type TransitionTypestate =
+  | {
+      value: 'hide'
+      context: TransitionContext
+    }
+  | {
+      value: 'slideIn'
+      context: TransitionContext
+    }
+  | {
+      value: 'visible'
+      context: TransitionContext
+    }
+  | {
+      value: 'slideOut'
+      context: TransitionContext
+    }
+
+const transitionMachine = createMachine<TransitionContext, TransitionEvent, TransitionTypestate>({
+  id: 'transition',
+  initial: 'hide',
+  states: {
+    hide: {
+      on: {
+        NEXT: { target: 'slideIn' },
+      },
+    },
+    slideIn: {
+      on: {
+        NEXT: { target: 'visible' },
+      },
+    },
+    visible: {
+      on: {
+        NEXT: { target: 'slideOut' },
+      },
+    },
+    slideOut: {
+      on: {
+        END: { target: 'hide' },
+        NEXT: { target: 'slideIn' },
+      },
+    },
+  },
+})
+
+const Track = ({ playing = false, title, artist, imageUrl }: Props): JSX.Element => {
+  const [state, send] = useMachine(transitionMachine)
+
+  const onAnimationEnd = () => {
+    state.matches('slideOut') && !playing ? send('END') : send('NEXT')
+  }
+
+  useEffect(() => {
+    if (playing) {
+      send('NEXT')
+    } else {
+      if (state.matches('visible')) {
+        send('NEXT')
+      }
+    }
+  }, [playing])
+
   return (
     <Container>
-      <StyledCoverJacket src={imageUrl} />
+      <Hidden>
+        <StyledCoverJacket state={state} src={imageUrl} onAnimationEnd={onAnimationEnd} />
+      </Hidden>
       <InfoContainer>
-        <Title>{title}</Title>
-        <Artist>{artist}</Artist>
+        <Hidden>
+          <Title state={state}>{title}</Title>
+        </Hidden>
+        <Hidden>
+          <Artist state={state}>{artist}</Artist>
+        </Hidden>
       </InfoContainer>
     </Container>
   )
 }
 
+const SlideIn = css`
+  animation: ${SlideLeft(true)} 0.7s ease-out 1s backwards;
+`
+const SlideOut = css`
+  animation: ${SlideLeft(false)} 0.7s ease-in 0.8s backwards;
+`
+
 const Container = styled.div`
-  display: grid;
+  display: inline-grid;
   grid-gap: 16px;
   grid-template-columns: auto 1fr;
 `
@@ -30,20 +116,77 @@ const InfoContainer = styled.div`
   gap: 8px;
 `
 
-const Title = styled.div`
+const Title = styled.div<{ state: TransitionState }>`
+  display: inline-block;
   font-size: 24px;
   font-weight: bold;
+
+  ${({ state }) => {
+    switch (state.value) {
+      case 'hide':
+        return css`
+          visibility: hidden;
+        `
+      case 'slideIn':
+        return SlideIn
+      case 'slideOut':
+        return SlideOut
+      case 'visible':
+        return css`
+          visibility: visible;
+        `
+    }
+  }}
 `
 
-const Artist = styled.div`
+const Artist = styled.div<{ state: TransitionState }>`
+  display: inline-block;
   font-size: 16px;
   color: #999;
+
+  ${({ state }) => {
+    switch (state.value) {
+      case 'hide':
+        return css`
+          visibility: hidden;
+        `
+      case 'slideIn':
+        return SlideIn
+      case 'slideOut':
+        return SlideOut
+      case 'visible':
+        return css`
+          visibility: visible;
+        `
+    }
+  }}
 `
 
-const StyledCoverJacket = styled.img`
+const StyledCoverJacket = styled.img<{ state: TransitionState }>`
   width: 80px;
   height: 80px;
   border-radius: 4px;
+
+  ${({ state }) => {
+    switch (state.value) {
+      case 'hide':
+        return css`
+          visibility: hidden;
+        `
+      case 'slideIn':
+        return SlideIn
+      case 'slideOut':
+        return SlideOut
+      case 'visible':
+        return css`
+          visibility: visible;
+        `
+    }
+  }}
+`
+
+const Hidden = styled.div`
+  overflow: hidden;
 `
 
 export default Track
